3db1c524b69df5be101132d59ae09df4d52bf7c5
Erwan Vallée <erwanp.vallee@gmail.com>
2021-01-15 19:47:50 +0100

    update

--- server/app.js
+++ server/app.js
@@ -1,29 +1,30 @@
-var express = require("express");
-var path = require("path");
-const mongoose = require("mongoose");
+var express = require('express');
+var path = require('path');
+const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    "mongodb+srv://jean:123@cluster0-urpjt.gcp.mongodb.net/chap21-react?retryWrites=true&w=majority",
+    'mongodb+srv://jean:123@cluster0-urpjt.gcp.mongodb.net/chap21-react?retryWrites=true&w=majority',
     {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
   )
   .then(() => {
-    console.log("CONNEXION DB OK !");
+    console.log('CONNEXION DB OK !');
   })
-  .catch(err => {
-    console.log("DB KO !");
+  .catch((err) => {
+    console.log('DB KO !');
   });
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "../client-build")));
+app.use(express.static(path.join(__dirname, '../client-build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client-build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client-build/index.html'));
 });
 
 module.exports = app;
